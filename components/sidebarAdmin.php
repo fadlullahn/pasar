4ad6ed10715c2bffe3e75e31e5bd76b35e274e18
fadlullan <fadlullahherifarid@gmail.com>
2024-08-17 04:41:41 +0800

    Deskripsi perubahan

--- components/sidebarAdmin.php
+++ components/sidebarAdmin.php
@@ -6,15 +6,16 @@
         <nav class="flex flex-1 flex-col">
             <div class="flex flex-1 flex-col gap-y-7">
                 <ul class="-mx-2 space-y-1">
+                    <li>
+                        <a id="dashboard" href="index.php" class="hover:bg-gray-800 text-gray-400 hover:text-white group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold">
+                            <svg class="h-6 w-6 shrink-0" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+                            </svg>
+                            Dashboard
+                        </a>
+                    </li>
                     <?php if (isset($_SESSION['level']) && $_SESSION['level'] == 'Admin') : ?>
-                        <li>
-                            <a id="dashboard" href="index.php" class="hover:bg-gray-800 text-gray-400 hover:text-white group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold">
-                                <svg class="h-6 w-6 shrink-0" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
-                                </svg>
-                                Dashboard
-                            </a>
-                        </li>
+
                         <li>
                             <a id="users" href="?page=users" class="text-gray-400 hover:text-white hover:bg-gray-800 group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold">
                                 <svg class="h-6 w-6 shrink-0" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
